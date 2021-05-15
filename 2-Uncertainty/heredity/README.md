4b32b325a08a123bb61be8a52dd9d23b6da4f1f8
Silas <silasyl@gmail.com>
2021-05-15 10:40:02 -0300

    heredity implemented with joint probabilities

---
+++ 2-Uncertainty/heredity/README.md
@@ -0,0 +1,7 @@
+# py-heredity
+An AI to assess the likelihood that a person will have a particular genetic trait.
+
+This app was implemented with joint probability to calculate probability of people having a certain number of genes and traits, based on GJB2 gene (hearing impairment in newborns) and is conditional on what genes their parents have.<br>
+This application can be expanded to other diseases and genetic patterns.
+
+Run: python heredity.py data/family0.csv
---
+++ 2-Uncertainty/heredity/main.py
@@ -0,0 +1,242 @@
+import csv
+import itertools
+import sys
+
+PROBS = {
+
+    # Unconditional probabilities for having gene
+    "gene": {
+        2: 0.01,
+        1: 0.03,
+        0: 0.96
+    },
+
+    "trait": {
+
+        # Probability of trait given two copies of gene
+        2: {
+            True: 0.65,
+            False: 0.35
+        },
+
+        # Probability of trait given one copy of gene
+        1: {
+            True: 0.56,
+            False: 0.44
+        },
+
+        # Probability of trait given no gene
+        0: {
+            True: 0.01,
+            False: 0.99
+        }
+    },
+
+    # Mutation probability
+    "mutation": 0.01
+}
+
+
+def main():
+
+    # Check for proper usage
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python heredity.py data.csv")
+    people = load_data(sys.argv[1])
+
+    print(f"people: {people}")
+
+    # Keep track of gene and trait probabilities for each person
+    probabilities = {
+        person: {
+            "gene": {
+                2: 0,
+                1: 0,
+                0: 0
+            },
+            "trait": {
+                True: 0,
+                False: 0
+            }
+        }
+        for person in people
+    }
+    # print(f"probabilities: {probabilities}")
+    people = {
+        'Harry': {'name': 'Harry', 'mother': 'Lily', 'father': 'James', 'trait': True},
+        'James': {'name': 'James', 'mother': None, 'father': None, 'trait': False},
+        'Lily': {'name': 'Lily', 'mother': None, 'father': None, 'trait': False}
+    }
+
+    # Loop over all sets of people who might have the trait
+    names = set(people)
+    # for have_trait in powerset(names):
+    # have_trait = "James"
+    # one_gene = "Harry"
+    # two_genes = "James"
+
+    have_trait = "Harry"
+    one_gene = {"James", "Lily"}
+    two_genes = "Harry"
+    p = joint_probability(people, one_gene, two_genes, have_trait)
+    print(p)
+    # update(probabilities, one_gene, two_genes, have_trait, p)
+    # print (probabilities)
+
+    # Ensure probabilities sum to 1
+    # normalize(probabilities)
+
+
+def load_data(filename):
+    """
+    Load gene and trait data from a file into a dictionary.
+    File assumed to be a CSV containing fields name, mother, father, trait.
+    mother, father must both be blank, or both be valid names in the CSV.
+    trait should be 0 or 1 if trait is known, blank otherwise.
+    """
+    data = dict()
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            name = row["name"]
+            data[name] = {
+                "name": name,
+                "mother": row["mother"] or None,
+                "father": row["father"] or None,
+                "trait": (True if row["trait"] == "1" else
+                          False if row["trait"] == "0" else None)
+            }
+    return data
+
+
+def powerset(s):
+    """
+    Return a list of all possible subsets of set s.
+    """
+    s = list(s)
+    return [
+        set(s) for s in itertools.chain.from_iterable(
+            itertools.combinations(s, r) for r in range(len(s) + 1)
+        )
+    ]
+
+
+def joint_probability(people, one_gene, two_genes, have_trait):
+    """
+    Compute and return a joint probability.
+
+    The probability returned should be the probability that
+        * everyone in set `one_gene` has one copy of the gene, and
+        * everyone in set `two_genes` has two copies of the gene, and
+        * everyone not in `one_gene` or `two_gene` does not have the gene, and
+        * everyone in set `have_trait` has the trait, and
+        * everyone not in set` have_trait` does not have the trait.
+    """
+    probability = 1
+    names = set(people)
+    for person in names:
+        if people[person]["mother"] != None:
+            if people[person]["mother"] in one_gene:
+                prob_m = 0.5
+                prob_nm = 0.5
+            elif people[person]["mother"] in two_genes:
+                prob_m = 1 - PROBS["mutation"]
+                prob_nm = PROBS["mutation"]
+            else:
+                prob_m = PROBS["mutation"]
+                prob_nm = 1 - PROBS["mutation"]
+
+            if people[person]["father"] in one_gene:
+                prob_f = 0.5
+                prob_nf = 0.5
+            elif people[person]["father"] in two_genes:
+                prob_f = 1 - PROBS["mutation"]
+                prob_nf = PROBS["mutation"]
+            else:
+                prob_f = PROBS["mutation"]
+                prob_nf = 1 - PROBS["mutation"]
+
+        if person in one_gene:
+            if people[person]["mother"] == None:
+                probability *= PROBS["gene"][1]
+            else:
+                print("one_gene")
+                print(prob_m)
+                print(prob_f)
+                probability *= prob_m * prob_nf + prob_nm * prob_f
+
+            if person in have_trait:
+                probability *= PROBS["trait"][1][True]
+            else:
+                probability *= PROBS["trait"][1][False]
+
+        elif person in two_genes:
+            if people[person]["mother"] == None:
+                probability *= PROBS["gene"][2]
+            else:
+                print("two genes")
+                print(prob_m)
+                print(prob_f)
+                probability *= prob_m * prob_f
+
+            if person in have_trait:
+                probability *= PROBS["trait"][2][True]
+            else:
+                probability *= PROBS["trait"][2][False]
+
+        else:
+            if people[person]["mother"] == None:
+                probability *= PROBS["gene"][0]
+            else:
+                print("no genes")
+                print(prob_m)
+                print(prob_f)
+                probability *= prob_nm * prob_nf
+
+            if person in have_trait:
+                probability *= PROBS["trait"][0][True]
+            else:
+                probability *= PROBS["trait"][0][False]
+
+    return probability
+
+
+def update(probabilities, one_gene, two_genes, have_trait, p):
+    """
+    Add to `probabilities` a new joint probability `p`.
+    Each person should have their "gene" and "trait" distributions updated.
+    Which value for each distribution is updated depends on whether
+    the person is in `have_gene` and `have_trait`, respectively.
+    """
+    for person in probabilities:
+        if person in one_gene:
+            probabilities[person]["gene"][1] += p
+        elif person in two_genes:
+            probabilities[person]["gene"][2] += p
+        else:
+            probabilities[person]["gene"][0] += p
+
+        if person in have_trait:
+            probabilities[person]["trait"][True] += p
+        else:
+            probabilities[person]["trait"][False] += p
+
+
+def normalize(probabilities):
+    """
+    Update `probabilities` such that each probability distribution
+    is normalized (i.e., sums to 1, with relative proportions the same).
+    """
+    for person in probabilities:
+        alpha = 1/(probabilities[person]["gene"][0] + probabilities[person]["gene"][1] + probabilities[person]["gene"][2])
+        probabilities[person]["gene"][0] *= alpha
+        probabilities[person]["gene"][1] *= alpha
+        probabilities[person]["gene"][2] *= alpha
+
+        alpha = 1 / (probabilities[person]["trait"][True] + probabilities[person]["trait"][False])
+        probabilities[person]["trait"][True] *= alpha
+        probabilities[person]["trait"][False] *= alpha
+
+
+if __name__ == "__main__":
+    main()
