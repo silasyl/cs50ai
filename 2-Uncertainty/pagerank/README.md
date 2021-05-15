619eea718b02c76359a5760dc2c67aed8438c201
Silas <silasyl@gmail.com>
2021-05-15 10:38:18 -0300

    PageRank implemented with sampling and iteration methods

---
+++ 2-Uncertainty/pagerank/README.md
@@ -0,0 +1,7 @@
+# py-pagerank
+An AI to rank web pages by importance
+
+This app implements Sampling method from a Markov Chain random surfer and Iteratively method applying the PageRank formula created by Google.<br>
+It works on search engine for Google display search results.
+
+Run: python pagerank.py corpus
---
+++ 2-Uncertainty/pagerank/pagerank.py
@@ -0,0 +1,147 @@
+import os
+import random
+import re
+import sys
+import numpy
+
+DAMPING = 0.85
+SAMPLES = 10000
+
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python pagerank.py corpus")
+    corpus = crawl(sys.argv[1])
+    ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
+    print(f"PageRank Results from Sampling (n = {SAMPLES})")
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
+    ranks = iterate_pagerank(corpus, DAMPING)
+    print(f"PageRank Results from Iteration")
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
+
+
+def crawl(directory):
+    """
+    Parse a directory of HTML pages and check for links to other pages.
+    Return a dictionary where each key is a page, and values are
+    a list of all other pages in the corpus that are linked to by the page.
+    """
+    pages = dict()
+
+    # Extract all links from HTML files
+    for filename in os.listdir(directory):
+        if not filename.endswith(".html"):
+            continue
+        with open(os.path.join(directory, filename)) as f:
+            contents = f.read()
+            links = re.findall(r"<a\s+(?:[^>]*?)href=\"([^\"]*)\"", contents)
+            pages[filename] = set(links) - {filename}
+
+    # Only include links to other pages in the corpus
+    for filename in pages:
+        pages[filename] = set(
+            link for link in pages[filename]
+            if link in pages
+        )
+
+    return pages
+
+
+def transition_model(corpus, page, damping_factor):
+    """
+    Return a probability distribution over which page to visit next,
+    given a current page.
+
+    With probability `damping_factor`, choose a link at random
+    linked to by `page`. With probability `1 - damping_factor`, choose
+    a link at random chosen from all pages in the corpus.
+    """
+    probability = dict()
+
+    # Damping factor:
+    if (len(corpus[page]) == 0):
+        for pages in corpus.keys():
+            probability[pages] = 1/len(corpus)
+        return probability
+    link_prob = damping_factor / len(corpus[page])
+    for link in corpus[page]:
+        probability[link] = link_prob
+
+    # 1 - Damping factor
+    all_prob = (1 - damping_factor) / len(corpus)
+    for pages in corpus.keys():
+        if pages in probability:
+            probability[pages] = probability[pages] + all_prob
+        else:
+            probability[pages] = all_prob
+
+    return probability
+
+
+def sample_pagerank(corpus, damping_factor, n):
+    """
+    Return PageRank values for each page by sampling `n` pages
+    according to transition model, starting with a page at random.
+
+    Return a dictionary where keys are page names, and values are
+    their estimated PageRank value (a value between 0 and 1). All
+    PageRank values should sum to 1.
+    """
+    page_rank = dict()
+    for pages in corpus.keys():
+        page_rank[pages] = 0
+
+    page = random.choice(list(corpus.keys()))
+    page_rank[page] += 1
+
+    for i in range(n - 1):
+        sample = transition_model(corpus, page, damping_factor)
+        sample_page = list(sample.keys())
+        sample_prob = list(sample.values())
+        page = numpy.random.choice(sample_page, p=sample_prob)
+        page_rank[page] += 1
+
+    for pages in corpus.keys():
+        page_rank[pages] /= n
+
+    return page_rank
+
+
+def iterate_pagerank(corpus, damping_factor):
+    """
+    Return PageRank values for each page by iteratively updating
+    PageRank values until convergence.
+
+    Return a dictionary where keys are page names, and values are
+    their estimated PageRank value (a value between 0 and 1). All
+    PageRank values should sum to 1.
+    """
+    page_rank = dict()
+    for pages in corpus.keys():
+        page_rank[pages] = 1 / len(corpus)
+    delta_page = dict()
+    delta = 1
+    while delta > 0.001:
+        for page in corpus.keys():
+            new_rank = calculate_pr(corpus, page, damping_factor, page_rank)
+            delta_page[page] = abs(new_rank - page_rank[page])
+            page_rank[page] = new_rank
+            delta = max(delta_page.values())
+            if delta < 0.001:
+                break
+    return page_rank
+
+
+def calculate_pr(corpus, p, d, pr):
+    sum = 0
+    for i in corpus.keys():
+        if p in corpus[i]:
+            sum += pr[i] / len(corpus[i])
+    probability = (1 - d) / len(corpus) + d * sum
+    return probability
+
+
+if __name__ == "__main__":
+    main()
